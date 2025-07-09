c0e70a3abc7a6ba15431922cf1046520ec729d78
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-09 11:41:20 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Maariya Qureshi on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
