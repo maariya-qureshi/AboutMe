82c47daa8da11b062358174b37923e73e49035de
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-09 11:59:50 -0400

    added 2 pictures, 2 captions, and 1 title

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //zstack for background color, allows for things to be on top of each other (images on the background color)
+        ZStack {
+            VStack { //to organize content vertically:
+                
+                Text(("all about maariya"))
+                    .font(.title)
+                
+                //2 H stacks to have pictures + verbs side by side:
+                HStack
+                {
+                    Image("painting")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("painting is my hobby")
+                }
+                
+                HStack
+                {
+                    Image("people")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("family & friends!")
+                }
+            }
         }
-        .padding()
     }
 }
 
